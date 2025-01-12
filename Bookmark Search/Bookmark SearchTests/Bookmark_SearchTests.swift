092ed5de1f3c657cc26a36d2d6cec9d7e278f44c
jkamin73 <jeff@jeffk.io>
2025-01-12 15:42:17 +0100

    Initial commit

---
+++ Bookmark Search/Bookmark SearchTests/Bookmark_SearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bookmark_SearchTests.swift
+//  Bookmark SearchTests
+//
+//  Created by Jeff Kaminsky on 1/12/25.
+//
+
+import Testing
+@testable import Bookmark_Search
+
+struct Bookmark_SearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Bookmark Search/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Jeff Kaminsky on 1/12/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
